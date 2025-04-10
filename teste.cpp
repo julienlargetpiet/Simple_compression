65e31630b39540e3770c157f26145050746a5e4b
Julien <pgyygp090@gmail.com>
2025-04-10 18:38:12 +0200

    removed commented code

--- teste.cpp
+++ teste.cpp
@@ -7,14 +7,10 @@ int main() {
   std::string out_file2 = "teste_file.txt";
   std::string k_file = "k";
   unsigned int n_pattern = 3;
-  //unsigned int cnt = 1;
   unsigned int level = 3;
 
   compression(inpt_file, n_pattern, k_file, level, out_file);
   decompression(out_file, k_file, out_file2);
-  //std::string teste = "12345";
-  //teste.erase(teste.begin() + 1, teste.begin() + 3);
-  //std::cout << teste << "\n";
 
   return 0;
 };
